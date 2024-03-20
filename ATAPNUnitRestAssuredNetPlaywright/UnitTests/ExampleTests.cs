89dd1a106c9b77a90bb6f6f4334395a5db923688
basdijkstra <bas@ontestautomation.com>
2024-03-20 16:46:44 +0100

    Initial commit - step 4

---
+++ ATAPNUnitRestAssuredNetPlaywright/UnitTests/ExampleTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace ATAPNUnitRestAssuredNetPlaywright.UnitTests
+{
+    [TestFixture]
+    public class ExampleTests
+    {
+        [Test]
+        public void Add2and2_CheckResult_ShouldBe4()
+        {
+            int result = 2 + 2;
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Add3and5_CheckResult_ShouldBe8()
+        {
+            int result = 3 + 5;
+
+            Assert.That(result, Is.EqualTo(7));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Reference project for a-test-automation-project
+
+This project is a reference implementation for the example test automation project [published here](https://github.com/basdijkstra/a-test-automation-project).
+
+This project uses:
+
+* C# as the programming language
+* NUnit as the test framework / test runner
+* RestAssured.Net as the API testing library
+* Playwright as the UI testing library
+* GitHub Actions as the CI platform
