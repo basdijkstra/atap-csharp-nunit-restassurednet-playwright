fe165592f3cf1c2e0434ca7c15dda1d48ac281fa
basdijkstra <bas@ontestautomation.com>
2024-05-29 15:23:03 +0200

    Create parameterized tests

--- ATAPNUnitRestAssuredNetPlaywright/ApiTests/ExampleApiTests.cs
+++ ATAPNUnitRestAssuredNetPlaywright/ApiTests/ExampleApiTests.cs
@@ -6,17 +6,20 @@ namespace ATAPNUnitRestAssuredNetPlaywright.ApiTests
     [TestFixture]
     public class ExampleApiTests
     {
-        [Test]
-        public void GetDataForUser1_CheckStatusCodeHeaderAndBody()
+        [TestCase(1, "Leanne Graham", "harness real-time e-markets", TestName = "User 1 is Leanne Graham")]
+        [TestCase(2, "Ervin Howell", "synergize scalable supply-chains", TestName = "User 2 is Ervin Howell")]
+        [TestCase(3, "Clementine Bauch", "e-enable strategic applications", TestName = "User 3 is Clementine Bauch")]
+        public void GetDataForUser_CheckStatusCodeHeaderAndBody
+            (int userId, string expectedName, string expectedBS)
         {
             Given()
                 .When()
-                .Get("https://jsonplaceholder.typicode.com/users/1")
+                .Get($"https://jsonplaceholder.typicode.com/users/{userId}")
                 .Then()
                 .StatusCode(200)
                 .ContentType("application/json; charset=utf-8")
-                .Body("$.name", NHamcrest.Is.EqualTo("Leanne Graham"))
-                .Body("$.company.bs", NHamcrest.Is.EqualTo("harness real-time e-markets"));
+                .Body("$.name", NHamcrest.Is.EqualTo(expectedName))
+                .Body("$.company.bs", NHamcrest.Is.EqualTo(expectedBS));
         }
 
         [Test]
