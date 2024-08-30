bc4173bb9fc2808364b380f6fea95c3aef08dd48
basdijkstra <bas@ontestautomation.com>
2024-08-30 13:16:15 +0200

    Add first Playwright test

---
+++ ATAPNUnitRestAssuredNetPlaywright/UiTests/ExampleUiTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace ATAPNUnitRestAssuredNetPlaywright.UiTests
+{
+    [TestFixture]
+    public class ExampleUiTests : PageTest
+    {
+        [Test]
+        public async Task CheckLoginToSauceDemoShop()
+        {
+            // Navigate to the Sauce Demo home page
+            await Page.GotoAsync("https://www.saucedemo.com");
+
+            // Perform the login sequence
+            await Page.GetByPlaceholder("Username").FillAsync("standard_user");
+            await Page.GetByPlaceholder("Password").FillAsync("secret_sauce");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+
+            // Check that the Sauce Labs Backpack product link is visible
+            await Expect(Page.GetByText("Sauce Labs Backpack")).ToBeVisibleAsync();
+        }
+    }
+}
